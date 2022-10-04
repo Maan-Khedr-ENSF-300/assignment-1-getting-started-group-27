afec05faa5c0b63d4ec72916c61adb36ce75a43f
mzafari2 <113462468+mzafari2@users.noreply.github.com>
2022-10-04 09:02:50 -0600

    name_function

--- assignment1.py
+++ assignment1.py
@@ -32,100 +32,100 @@ def input_op(x):
         x = input("\n\nSelection invalid. Choose an option from the list. \n\nSelect an operator:\n*\t/\t+\t-\n")
         return x
 
-
-print("\nSelect three integers and 2 operators as requested.\n")
-
-
-a = input_int()
-print('\na =', a)
-
-operator1 = input("\nSelect an operator:\n*\t/\t+\t-\n")
-input_op(operator1)                         
-if operator1 == '*':                            #first operation is multipication
-    print('\nOperation 1: Multiply\n')
-elif operator1 == '/':                          #first operation is division 
-    print('\nOperation 1: Divide\n')
-elif operator1 == '+':                          #first operation is adding
-    print('\nOperation 1: Add\n')               
-elif operator1 == '-':                          #first operation is subtracting
-    print('\nOperation 1: Subtract\n')          
-
-b = input_int()
-print('\nb =', b)
-if operator1 == '2' and b == 0:  ### if dividing by zero, print 'undefined' and ask for new integer.
-        b = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
-print('\nb =', b)
-
-operator2 = input("\nSelect an operator:\n*\t/\t+\t-\n")
-input_op(operator2)
-if operator2 == '*':                              #second operation is multipication
-    print('\nOperation 2: Multiply\n')
-elif operator2 == '/':                            #second operation is divison 
-    print('\nOperation 2: Divide\n')
-elif operator2 == '+':                            #second operation is adding
-    print('\nOperation 2: Add\n')
-elif operator2 == '-':                            #second operation is subtracting
-    print('\nOperation 2: Subtract\n')
-
-c = input_int()
-if operator2 == '2' and c == 0:     ### if dividing by zero, print 'undefined' and ask for new integer.
-        c = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
-print('\nc =', c)
+def main():
+    print("\nSelect three integers and 2 operators as requested.\n")
 
 
+    a = input_int()
+    print('\na =', a)
 
-if operator1 == '*' and operator2 == '*':
-    print(a, '*', b, '*', c, '=', myMul(a, b, c))
-elif operator1 == '1' and operator2 == '/':
-        d = myMul(a, b)
-        print(a, '*', b, '/', c, '=', myDiv(d, c))
-elif operator1 == '*' and operator2 == '+':
-        d = myMul(a, b)
-        print(a, '*', b, '+', c, '=', myAdd(d, c))
-elif operator1 == '*' and operator2 == '-':
-        d = myMul(a, b)
-        print(a, '*', b, '-', c, '=', mySub(d, c))
+    operator1 = input("\nSelect an operator:\n*\t/\t+\t-\n")
+    input_op(operator1)                         
+    if operator1 == '*':                            #first operation is multipication
+        print('\nOperation 1: Multiply\n')
+    elif operator1 == '/':                          #first operation is division 
+        print('\nOperation 1: Divide\n')
+    elif operator1 == '+':                          #first operation is adding
+        print('\nOperation 1: Add\n')               
+    elif operator1 == '-':                          #first operation is subtracting
+        print('\nOperation 1: Subtract\n')          
 
-elif operator1 == '/' and operator2 == '*':
-        d = myMul(b, c)
-        print(a, '/', b, '*', c, '=', myDiv(a, d))
-elif operator1 == '/' and operator2 == '/':
-        d = myDiv(a, b)
-        print(a, '/', b, '/', c, '=', myDiv(d, c)) 
-elif operator1 == '/' and operator2 == '+':
-        d = myDiv(a, b)
-        print(a, '/', b, '+', c, '=', myAdd(d, c))      
-elif operator1 == '/' and operator2 == '-':
-        d = myDiv(a, b)
-        print(a, '/', b, '-', c, '=', mySub(d, c))  
+    b = input_int()
+    print('\nb =', b)
+    if operator1 == '2' and b == 0:  ### if dividing by zero, print 'undefined' and ask for new integer.
+            b = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
+    print('\nb =', b)
 
-elif operator1 == '+' and operator2 == '*':
-        d = myMul(b, c)
-        print(a, '+', b, '*', c, '=', myAdd(a, d))
-elif operator1 == '+' and operator2 == '/':
-        d = myDiv(b, c)
-        print(a, '+', b, '/', c, '=', myAdd(a, d))
-elif operator1 == '+' and operator2 == '+':
-        d = myAdd(b, c)
-        print(a, '+', b, '+', c, '=', myAdd(a, d))
-elif operator1 == '+' and operator2 == '-':
-        d = mySub(b, c)
-        print(a, '+', b, '-', c, '=', myAdd(a, d))
+    operator2 = input("\nSelect an operator:\n*\t/\t+\t-\n")
+    input_op(operator2)
+    if operator2 == '*':                              #second operation is multipication
+        print('\nOperation 2: Multiply\n')
+    elif operator2 == '/':                            #second operation is divison 
+        print('\nOperation 2: Divide\n')
+    elif operator2 == '+':                            #second operation is adding
+        print('\nOperation 2: Add\n')
+    elif operator2 == '-':                            #second operation is subtracting
+        print('\nOperation 2: Subtract\n')
 
-elif operator1 == '-' and operator2 == '*':
-        d = myMul(b, c)
-        print(a, '-', b, '*', c, '=', mySub(a, d))
-elif operator1 == '-' and operator2 == '/':
-        d = myDiv(b, c)
-        print(a, '-', b, '/', c, '=', mySub(a, d))
-elif operator1 == '-' and operator2 == '+':
-        d = mySub(a, b)
-        print(a, '-', b, '+', c, '=', myAdd(d, c))
-elif operator1 == '-' and operator2 == '-':
-        d = mySub(a, b)
-        print(a, '-', b, '-', c, '=', mySub(d, c))
+    c = input_int()
+    if operator2 == '2' and c == 0:     ### if dividing by zero, print 'undefined' and ask for new integer.
+            c = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
+    print('\nc =', c)
 
 
 
+    if operator1 == '*' and operator2 == '*':
+        print(a, '*', b, '*', c, '=', myMul(a, b, c))
+    elif operator1 == '1' and operator2 == '/':
+            d = myMul(a, b)
+            print(a, '*', b, '/', c, '=', myDiv(d, c))
+    elif operator1 == '*' and operator2 == '+':
+            d = myMul(a, b)
+            print(a, '*', b, '+', c, '=', myAdd(d, c))
+    elif operator1 == '*' and operator2 == '-':
+            d = myMul(a, b)
+            print(a, '*', b, '-', c, '=', mySub(d, c))
 
+    elif operator1 == '/' and operator2 == '*':
+            d = myMul(b, c)
+            print(a, '/', b, '*', c, '=', myDiv(a, d))
+    elif operator1 == '/' and operator2 == '/':
+            d = myDiv(a, b)
+            print(a, '/', b, '/', c, '=', myDiv(d, c)) 
+    elif operator1 == '/' and operator2 == '+':
+            d = myDiv(a, b)
+            print(a, '/', b, '+', c, '=', myAdd(d, c))      
+    elif operator1 == '/' and operator2 == '-':
+            d = myDiv(a, b)
+            print(a, '/', b, '-', c, '=', mySub(d, c))  
+
+    elif operator1 == '+' and operator2 == '*':
+            d = myMul(b, c)
+            print(a, '+', b, '*', c, '=', myAdd(a, d))
+    elif operator1 == '+' and operator2 == '/':
+            d = myDiv(b, c)
+            print(a, '+', b, '/', c, '=', myAdd(a, d))
+    elif operator1 == '+' and operator2 == '+':
+            d = myAdd(b, c)
+            print(a, '+', b, '+', c, '=', myAdd(a, d))
+    elif operator1 == '+' and operator2 == '-':
+            d = mySub(b, c)
+            print(a, '+', b, '-', c, '=', myAdd(a, d))
+
+    elif operator1 == '-' and operator2 == '*':
+            d = myMul(b, c)
+            print(a, '-', b, '*', c, '=', mySub(a, d))
+    elif operator1 == '-' and operator2 == '/':
+            d = myDiv(b, c)
+            print(a, '-', b, '/', c, '=', mySub(a, d))
+    elif operator1 == '-' and operator2 == '+':
+            d = mySub(a, b)
+            print(a, '-', b, '+', c, '=', myAdd(d, c))
+    elif operator1 == '-' and operator2 == '-':
+            d = mySub(a, b)
+            print(a, '-', b, '-', c, '=', mySub(d, c))
+
+
+if __name__ == '__main__':
+    main()
 
