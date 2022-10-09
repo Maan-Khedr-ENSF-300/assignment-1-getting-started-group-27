793f19aaa8d57942adf41d9c4bcf868ab6a31344
macaylak <macayla.konig@ucalgary.ca>
2022-10-08 22:48:05 -0600

    small edits

--- assignment1.py
+++ assignment1.py
@@ -131,67 +131,10 @@ def main():
             c = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
     
 
-<<<<<<< HEAD
-
-
     calculator(a, b, c, operator1, operator2)
 
 if __name__ == '__main__':
     main()
 
 
-=======
-## A function that validates user input for operator1 and operator2
-def input_op(x): 
-    while x not in {'*', '/', '+', '-'}:
-        x = input("\n\nSelection invalid. Choose an option from the list. \n\nSelect an operator:\n*\t/\t+\t-\n")
-        return x
-
-def main():
-    print("\nSelect three integers and 2 operators as requested.\n")
-
-
-    a = input_int() # request input for first integer
-    print('\na =', a) # validate user input
-
-    operator1 = input("\nSelect an operator:\n*\t/\t+\t-\n") # requests user input for first operator
-    input_op(operator1) # validate user input                       
-    if operator1 == '*':                            #first operation is multipication
-        print('\nOperation 1: Multiply\n')
-    elif operator1 == '/':                          #first operation is division 
-        print('\nOperation 1: Divide\n')
-    elif operator1 == '+':                          #first operation is adding
-        print('\nOperation 1: Add\n')               
-    elif operator1 == '-':                          #first operation is subtracting
-        print('\nOperation 1: Subtract\n')          
-
-    b = input_int() ## request user input for second integer
-    print('\nb =', b) ## validate user input
-    if operator1 == '2' and b == 0:  ### if dividing by zero, print 'undefined' and ask for new integer.
-            b = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
-    print('\nb =', b)
-
-    operator2 = input("\nSelect an operator:\n*\t/\t+\t-\n") # requests user input for second operator
-    input_op(operator2) ## validates user input
-    if operator2 == '*':                              #second operation is multipication
-        print('\nOperation 2: Multiply\n')
-    elif operator2 == '/':                            #second operation is divison 
-        print('\nOperation 2: Divide\n')
-    elif operator2 == '+':                            #second operation is adding
-        print('\nOperation 2: Add\n')
-    elif operator2 == '-':                            #second operation is subtracting
-        print('\nOperation 2: Subtract\n')
-
-    c = input_int() ## request user input for third integer
-    if operator2 == '2' and c == 0:     ### if dividing by zero, print 'undefined' and ask for new integer.
-            c = int(input('Undefined. Select an integer greater than 0 for valid operation:\n'))
-    print('\nc =', c)
-
-
-
-    
-
-if __name__ == '__main__':
-    main()
->>>>>>> a9282ffd9edf4ed446c1abe714423e5f179daee6
 
